99a33b48deedb543f77f50ebc9c24c8d7b113cae
Luca <schulzwitt.janluca@gmail.com>
2023-04-20 16:37:10 +0200

    Added Engine Terminal Function

--- Code Breaker/Assets/Scripts/CodeTerminal.cs
+++ Code Breaker/Assets/Scripts/CodeTerminal.cs
@@ -10,38 +10,60 @@ public class CodeTerminal : MonoBehaviour
     [SerializeField] private float maxInput = 4; //Maximale Eingabe vom Spieler
     [SerializeField] private float currentInput = 0; //Aktuelle Eingabe vom Spieler
     [SerializeField] private bool isComplete = false; //Wurde das Rätsel gelöst?
+    [SerializeField] private float waitTime = 1;
+
+    [Header("Turbine")]
+    [SerializeField] private AudioSource source;
+    [SerializeField] private AudioClip EngineSoundFixed;
+    [SerializeField] private Animator TurbineAnimator;
 
     [SerializeField] private TextMeshProUGUI codeText;
+    [SerializeField] private GameObject Checkmark;
+    [SerializeField] private GameObject Cross;
 
     private void Update()
     {
-        //Wenn die Eingabe des Spieler größer gleich die maximale Eingabe des Spieler ist und das Rätsel nicht gelöst wurde dann:
-        if(currentInput >= maxInput && !isComplete)
-        {
-            if (codeInput == code) //Wenn der eingegebene Code der gleiche ist wie der des Rätsels dann:
-            {
-                Debug.Log("Win");
-            }
-            else //Wenn der Code nicht richtig ist dann:
-            {
-                Lose(); //Startet Funktion
-            }
-        }
-
         UpdateUI();
+    }
+
+    public void CheckInput()
+    {
+        if (codeInput == code) //Wenn der eingegebene Code der gleiche ist wie der des Rätsels dann:
+        {
+            codeText.gameObject.SetActive(false);
+            Checkmark.SetActive(true);
+            TurbineAnimator.SetTrigger("Fix");
+            source.Stop();
+            source.PlayOneShot(EngineSoundFixed);
+            Debug.Log("Win");
+        }
+        else //Wenn der Code nicht richtig ist dann:
+        {
+            StartCoroutine(Lose()); //Startet Funktion
+        }
     }
 
     public void TerminalInput(int input)
     {
-        currentInput += 1;
-        codeInput += input;
+        if (currentInput < maxInput)
+        {
+            currentInput += 1;
+            codeInput += input;
+        }
     }
 
-        private void Lose()
+    IEnumerator Lose()
     {
+        codeText.gameObject.SetActive(false);
+        Cross.SetActive(true);
+        FindObjectOfType<AudioManager>().PlayAudio("HackGame_Fail");
         currentInput = 0; //Spielereingabe wird auf 0 gesetzt
         codeInput = ""; //Spielereingabe wird gelöscht
+        yield return new WaitForSeconds(waitTime);
+        codeText.gameObject.SetActive(true);
+        Cross.SetActive(false);
         Debug.Log("Lose");
+        yield return null;
     }
 
     private void UpdateUI()
